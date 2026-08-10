21429a7779d25c5de2094c39cfe3b7bae1321fdd
Daniel Abeles <dabeles@gitlab.com>
2026-08-10 23:39:43 +0300

    Replace dead Oxeye blog links with Wayback Machine URLs
    oxeye.io now 301-redirects everything to about.gitlab.com after the
    acquisition, so all six Oxeye blog posts were unreachable. Point them
    at verified Wayback Machine snapshots (using the full original slugs,
    which the previous links had truncated).

--- sveltekit-site/src/routes/+page.svelte
+++ sveltekit-site/src/routes/+page.svelte
@@ -47,12 +47,12 @@
 
 	const blogPosts = [
 		{ title: 'GitLab discovers widespread npm supply chain attack', url: 'https://about.gitlab.com/blog/gitlab-discovers-widespread-npm-supply-chain-attack/' },
-		{ title: 'Take Me to Prom - Exploiting RCE in openTSDB through Prometheus', url: 'https://www.oxeye.io/resources/take-me-to-prom' },
-		{ title: 'Gophers & Bees - Parsing Golang structures with eBPF', url: 'https://www.oxeye.io/resources/parsing-golang-structures' },
-		{ title: 'RCE through SQL Injection in Hashicorp Vault', url: 'https://www.oxeye.io/resources/rce-through-sql-injection' },
-		{ title: 'BreakStage - Unauthenticated RCE in Spotify Backstage', url: 'https://www.oxeye.io/resources/remote-code-execution-in-spotifys-backstage' },
-		{ title: 'Sandbreak - VM2 Sandbox Escape (CVE-2022-36067)', url: 'https://www.oxeye.io/resources/vm2-sandbreak-vulnerability' },
-		{ title: 'ParseThru - HTTP Parameter Smuggling in Golang', url: 'https://www.oxeye.io/resources/golang-parameter-smuggling' }
+		{ title: 'Take Me to Prom - Exploiting RCE in openTSDB through Prometheus', url: 'https://web.archive.org/web/20231001020456/https://www.oxeye.io/resources/take-me-to-prom-exploiting-an-rce-in-opentsdb-through-prometheus' },
+		{ title: 'Gophers & Bees - Parsing Golang structures with eBPF', url: 'https://web.archive.org/web/20231001024726/https://www.oxeye.io/resources/parsing-golang-structures-in-memory-with-ebpf' },
+		{ title: 'RCE through SQL Injection in Hashicorp Vault', url: 'https://web.archive.org/web/20230528000205/https://www.oxeye.io/resources/rce-through-sql-injection-vulnerability-in-hashicorps-vault' },
+		{ title: 'BreakStage - Unauthenticated RCE in Spotify Backstage', url: 'https://web.archive.org/web/20230527233012/https://www.oxeye.io/resources/remote-code-execution-in-spotifys-backstage' },
+		{ title: 'Sandbreak - VM2 Sandbox Escape (CVE-2022-36067)', url: 'https://web.archive.org/web/20230528002552/https://www.oxeye.io/resources/vm2-sandbreak-vulnerability-cve-2022-36067' },
+		{ title: 'ParseThru - HTTP Parameter Smuggling in Golang', url: 'https://web.archive.org/web/20230527224815/https://www.oxeye.io/resources/golang-parameter-smuggling-attack' }
 	];
 
 	const skills = [
